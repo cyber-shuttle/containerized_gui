ce0981ad6eb8fc6d3ff278ac327ee6ad414ac16f
Marcus Christie <machristie@apache.org>
2022-09-16 12:18:55 -0400

    Python script for parsing strace log for output files written

---
+++ parse_strace.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import fileinput
+import json
+import re
+
+# Only consider files that are written
+syscall_line = re.compile(r"\d+\s+open\w*\(\w+, \"([^\"]+)\".*(O_WRONLY|O_RDWR)")
+file_exclusions = re.compile(r"/root/.config|/root/.cache|/root/.local|/dev")
+
+
+def get_output_files(log_file):
+    output_file_paths = set()
+    for line in log_file:
+        line = line.strip()
+
+        m = syscall_line.match(line)
+        if m:
+            file_path = m.group(1)
+            if not file_exclusions.match(file_path):
+                output_file_paths.add(file_path)
+    return output_file_paths
+
+
+if __name__ == "__main__":
+    output_file_paths = get_output_files(fileinput.input())
+
+    print(json.dumps({"output-files": list(output_file_paths)}))
