26f0e42439db5667fc2753aaaca79d8de6549e07
Marcus Christie <machristie@apache.org>
2022-09-20 15:29:23 -0400

    Add autoconnect and password parameters to url
    also cleaned up console messages

--- driver.gimp.py
+++ driver.gimp.py
@@ -3,7 +3,6 @@
 import json
 import os
 import shutil
-import socket
 import subprocess
 import sys
 from tempfile import tempdir
@@ -19,7 +18,6 @@ if __name__ == "__main__":
         print("Missing filename of input file")
         sys.exit(1)
     input_file = sys.argv[1]
-    print(input_file)
 
     # Create a data directory and copy input file into it
     with tempfile.TemporaryDirectory() as data_volume:
@@ -57,31 +55,35 @@ if __name__ == "__main__":
                 "localhost:5900",
             ],
             stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
             cwd="./noVNC-1.3.0/utils/",
         )
         for line in novnc_proc.stdout:
             line = line.decode()
+            # print(line)
             if line.strip().startswith("http"):
-                webbrowser.open(line.strip())
+                novnc_url = line.strip() + "&autoconnect=1&password=1234"
+                print(f"opening browser tab for {novnc_url}")
+                webbrowser.open(novnc_url)
                 break
-            print(line)
         # Wait for container to exit
         docker_proc.wait()
         novnc_proc.terminate()
         # Parse stdout and get output file paths
         container_output = docker_proc.stdout.read().decode()
-        print(f"{container_output=}")
+        # print(f"{container_output=}")
         container_output_dict = json.loads(container_output)
         output_file_paths = container_output_dict["output-files"]
         docker_container_id = container_output_dict["container-id"]
-        print(f"{output_file_paths=}")
-        # Retrieve output file from container
-        subprocess.check_call(
-            [
-                "docker",
-                "cp",
-                f"{docker_container_id}:{output_file_paths[0]}",
-                "./output",
-            ]
-        )
+        for output_file_path in output_file_paths:
+            # Retrieve output file from container
+            print(f"copying {os.path.basename(output_file_path)} to ./output")
+            subprocess.check_call(
+                [
+                    "docker",
+                    "cp",
+                    f"{docker_container_id}:{output_file_path}",
+                    "./output",
+                ]
+            )
         # TODO: Remove docker container
