1dba8e16fe5ba96b61475d0f0482bc63fa401647
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2022-10-04 00:49:33 -0400

    Automatically select the file in /inputs to open in gimp

--- startx11vnc.gimp.sh
+++ startx11vnc.gimp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 unzip /opt/ARCHIVE.zip -d /inputs
+FILES=`ls /inputs`
 export DISPLAY=:0
 Xvfb "$DISPLAY" -screen 0 1024x768x24 &
-gimp "$@" >& /dev/null &
+gimp /inputs/$FILES >& /dev/null &
 echo $! > /tmp/gui.pid
 /usr/bin/x11vnc -display "$DISPLAY" -usepw -forever -bg >& /dev/null
 exec /start_strace.sh
