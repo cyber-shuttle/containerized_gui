fbd301f4c301b7bf2321436e5fb941bf487435ea
Marcus Christie <machristie@apache.org>
2022-10-26 23:32:59 -0400

    Encapsulate GUI container in a thread that can be terminated

--- containerized_gui/decorator.py
+++ containerized_gui/decorator.py
@@ -8,7 +8,149 @@ import shutil
 import subprocess
 import tempfile
 import threading
+from uuid import uuid4
 import webbrowser
+
+
+class ContainerizedGUIThread(threading.Thread):
+    def __init__(
+        self,
+        input_file,
+        image_name,
+        *args,
+        vnc_url_handler=None,
+        output_files_list=None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.input_file = input_file
+        self.image_name = image_name
+        self.vnc_url_handler = vnc_url_handler
+        if output_files_list is not None:
+            self.output_files = output_files_list
+        else:
+            self.output_files = []
+        self.kill_event = threading.Event()
+
+    def run(self) -> None:
+        print("inside run")
+
+        with tempfile.TemporaryDirectory() as data_volume:
+            filename = os.path.basename(self.input_file)
+            shutil.copy(self.input_file, os.path.join(data_volume, filename))
+
+            docker_container_name = str(uuid4())
+            docker_proc = subprocess.Popen(
+                [
+                    "docker",
+                    "run",
+                    "-p",
+                    # TODO: Can this be dynamic?
+                    "5900:5900",
+                    "-v",
+                    f"{data_volume}:/data",
+                    "--cap-add=SYS_PTRACE",
+                    "--name",
+                    docker_container_name,
+                    self.image_name,
+                    f"/data/{filename}",
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+
+            # TODO: wait for docker VNC server to be ready
+
+            # This doesn't work, probably executes too soon?
+            # docker_container_id = (
+            #     subprocess.check_output(["docker", "ps", "-l", "--quiet"]).decode().strip()
+            # )
+            # print(f"{docker_container_id=}")
+
+            # Start a VNC viewer, pointed at container
+            novnc_proc = subprocess.Popen(
+                [
+                    "./novnc_proxy",
+                    "--vnc",
+                    "localhost:5900",
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.DEVNULL,
+                cwd="./noVNC-1.3.0/utils/",
+            )
+            for line in novnc_proc.stdout:
+                line = line.decode()
+                # print(line)
+                if line.strip().startswith("http"):
+                    novnc_url = line.strip() + "&autoconnect=1&password=1234"
+                    if self.vnc_url_handler is not None:
+                        self.vnc_url_handler(novnc_url)
+                    else:
+                        print(f"opening browser tab for {novnc_url}")
+                        webbrowser.open(novnc_url)
+                    break
+            # Wait for container to exit
+            print("noVNC is running now")
+            returncode = docker_proc.poll()
+            while not self.kill_event.is_set() and returncode is None:
+                try:
+                    print(
+                        f"waiting for docker container {docker_container_name=}, {returncode=}"
+                    )
+                    returncode = docker_proc.wait(1)
+                except subprocess.TimeoutExpired:
+                    pass
+            else:
+                # if container hasn't exited, then we're trying to kill it, so forceably stop it
+                if returncode is None:
+                    subprocess.run(
+                        ["docker", "stop", "--time", "1", docker_container_name],
+                        capture_output=True,
+                    )
+            novnc_proc.terminate()
+            print("terminated noVNC")
+            try:
+                # Parse stdout and get output file paths
+                container_output = docker_proc.stdout.read().decode()
+                # print(f"{container_output=}")
+                container_output_dict = json.loads(container_output)
+                output_file_paths = container_output_dict["output-files"]
+                docker_container_id = container_output_dict["container-id"]
+                working_dir = container_output_dict["working-dir"]
+                result = []
+                for output_file_path in output_file_paths:
+                    # Retrieve output file from container
+                    output_filename = os.path.basename(output_file_path)
+                    # Some paths are relative to the container working directory
+                    full_output_file_path = (
+                        output_file_path
+                        if os.path.isabs(output_file_path)
+                        else os.path.join(working_dir, output_file_path)
+                    )
+                    print(f"copying {output_filename} to ./output")
+                    copy_result = subprocess.run(
+                        [
+                            "docker",
+                            "cp",
+                            f"{docker_container_id}:{full_output_file_path}",
+                            "./output",
+                        ]
+                    )
+                    # verify that copy succeeds
+                    if copy_result.returncode == 0:
+                        result.append(os.path.join(".", "output", output_filename))
+                self.output_files = result
+            except:
+                pass
+            finally:
+                # Remove docker container
+                print("removing docker container")
+                subprocess.run(
+                    ["docker", "rm", "-f", docker_container_name], capture_output=True
+                )
+
+    def kill(self):
+        self.kill_event.set()
 
 
 def run_gui(input_file, image_name, vnc_url_handler=None):
--- containerized_gui/widgets.py
+++ containerized_gui/widgets.py
@@ -1,14 +1,23 @@
 import io
 import threading
 from contextlib import redirect_stdout
+from types import MethodType
 
 import ipywidgets as widgets
 from IPython.display import IFrame
 from traitlets import List, Unicode
 
-from containerized_gui.decorator import run_gui
+from containerized_gui.decorator import ContainerizedGUIThread, run_gui
 
 FILE = object()
+
+
+def shutdown(self):
+    if self.thread.is_alive():
+        self.thread.kill()
+        # join to thread to wait for it to finish
+        self.thread.join()
+
 
 # TODO: add a run button
 # TODO: input file selector
@@ -25,23 +34,38 @@ def GUIContainer(
 ):
     out = widgets.Output(layout={"border": "1px solid black"})
     out.output_files = List(trait=Unicode())
+    out.shutdown = MethodType(shutdown, out)
 
     # create vnc url handler, write iframe to output widget
     def vnc_url_handler(vnc_url):
         out.append_display_data(IFrame(vnc_url, width, height))
 
-    def run_gui_thread(input_file):
-        output_files = run_gui(input_file, image_name, vnc_url_handler=vnc_url_handler)
-        out.output_files = output_files
-        if output_files_handler is not None:
-            # Capture any output from wrapped function and write to output widget
-            with redirect_stdout(io.StringIO()) as stdout:
-                output_files_handler(output_files)
-        # Clear output widget (closes VNC iframe)
-        # (See https://github.com/jupyter-widgets/ipywidgets/issues/3260#issuecomment-907715980 for this workaround)
-        out.outputs = ()
-        out.append_stdout(stdout.getvalue())
+    # def run_gui_thread(input_file):
+    #     output_files = run_gui(input_file, image_name, vnc_url_handler=vnc_url_handler)
+    #     out.output_files = output_files
+    #     if output_files_handler is not None:
+    #         # Capture any output from wrapped function and write to output widget
+    #         with redirect_stdout(io.StringIO()) as stdout:
+    #             output_files_handler(output_files)
+    #     # Clear output widget (closes VNC iframe)
+    #     # (See https://github.com/jupyter-widgets/ipywidgets/issues/3260#issuecomment-907715980 for this workaround)
+    #     out.outputs = ()
+    #     out.append_stdout(stdout.getvalue())
 
-    thread = threading.Thread(target=run_gui_thread, args=(input_file,))
-    thread.start()
+    # thread = threading.Thread(target=run_gui_thread, args=(input_file,))
+    out.thread = ContainerizedGUIThread(
+        input_file,
+        image_name,
+        vnc_url_handler=vnc_url_handler,
+    )
+    out.thread.start()
+    print("thread started")
+
+    def cleanup():
+        out.thread.join()
+        out.output_files = out.thread.output_files
+        out.outputs = ()
+
+    threading.Thread(target=cleanup).start()
+
     return out
